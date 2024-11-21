4a5d1367d578e8e6fb7cfb02d3d66fe140038017
Prashant Channe <prashantchanne121@gmail.com>
2024-11-21 14:09:35 -0500

    created dialog

--- src/app/components/anatomical-structure-details/anatomical-structure-details.component.ts
+++ src/app/components/anatomical-structure-details/anatomical-structure-details.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { StructureDetails } from '../../models/anatomical-structure-detail-model';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-anatomical-structure-details',
-  imports: [],
   templateUrl: './anatomical-structure-details.component.html',
-  styleUrl: './anatomical-structure-details.component.scss'
+  styleUrls: ['./anatomical-structure-details.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatDialogModule],
 })
 export class AnatomicalStructureDetailsComponent {
+  constructor(
+    public dialogRef: MatDialogRef<AnatomicalStructureDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: StructureDetails
+  ) {}
 
+  onClose(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/components/anatomical-structures/anatomical-structures.component.ts
+++ src/app/components/anatomical-structures/anatomical-structures.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
-import { Structure } from '../../models/anatomical-structure-model';
+import { Component, OnInit } from '@angular/core';
 import { AnatomicalStructureService } from '../../services/anatomical-structure.service';
+import { AnatomicalStructureDetailsService } from '../../services/anatomical-structure-details.service';
+import { Structure } from '../../models/anatomical-structure-model';
+import { StructureDetails } from '../../models/anatomical-structure-detail-model';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { AnatomicalStructureDetailsComponent } from '../anatomical-structure-details/anatomical-structure-details.component';
 @Component({
   selector: 'app-anatomical-structures',
-  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './anatomical-structures.component.html',
-  styleUrl: './anatomical-structures.component.scss',
+  styleUrls: ['./anatomical-structures.component.scss'],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatDialogModule],
 })
-export class AnatomicalStructuresComponent {
+export class AnatomicalStructuresComponent implements OnInit {
   uniqueStructures: Structure[] = [];
+  selectedStructureDetails?: StructureDetails;
 
-  constructor(private anatomicalStructureService: AnatomicalStructureService) {}
+  constructor(
+    private anatomicalStructureService: AnatomicalStructureService,
+    private anatomicalStructureDetailsService: AnatomicalStructureDetailsService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.anatomicalStructureService.fetchData().subscribe((data) => {
@@ -31,12 +39,33 @@ export class AnatomicalStructuresComponent {
       this.uniqueStructures = Array.from(uniqueNames)
         .map((name) => structures.find((s) => s.name === name)!)
         .filter((s): s is Structure => s !== undefined);
-
-      console.log(this.uniqueStructures);
     });
   }
 
   onStructureClick(structure: Structure) {
-    console.log('Clicked structure:', structure);
+    if (structure.id) {
+      this.anatomicalStructureDetailsService
+        .fetchDetails(structure.id?.replace(':', '_')!)
+        .subscribe((details) => {
+          this.openDetailsDialog(details);
+        });
+    } else {
+      this.openDetailsDialog();
+    }
+  }
+
+  openDetailsDialog(details?: StructureDetails) {
+    const dialogRef = this.dialog.open(AnatomicalStructureDetailsComponent, {
+      width: '500px',
+      data: details,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  closeDetails() {
+    this.selectedStructureDetails = undefined;
   }
 }
