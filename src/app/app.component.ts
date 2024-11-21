b71a130e32790cdf36f04267297190a3913284a0
Prashant Channe <prashantchanne121@gmail.com>
2024-11-21 12:47:39 -0500

    created basic anatomical structure list page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { AnatomicalStructureService } from './services/anatomical-structure.service';
-import { Structure } from './models/anatomical-structure-model';
+import { AnatomicalStructuresComponent } from './components/anatomical-structures/anatomical-structures.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatToolbarModule],
+  imports: [RouterOutlet, AnatomicalStructuresComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'cns';
-  uniqueStructures: string[] = [];
-
-  constructor(private anatomicalStructureService: AnatomicalStructureService) {}
-
-  ngOnInit() {
-    this.anatomicalStructureService.fetchData().subscribe((data) => {
-      const structures: Structure[] = data.data
-        .map((row: any) => row.anatomical_structures)
-        .flat();
-
-      this.uniqueStructures = Array.from(
-        new Set(
-          structures
-            .map((s) => s.name)
-            .filter(
-              (name): name is string => name !== null && name !== undefined
-            )
-        )
-      );
-      console.log(this.uniqueStructures);
-    });
-  }
-}
+export class AppComponent {}
--- src/app/components/anatomical-structures/anatomical-structures.component.ts
+++ src/app/components/anatomical-structures/anatomical-structures.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { Structure } from '../../models/anatomical-structure-model';
+import { AnatomicalStructureService } from '../../services/anatomical-structure.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-anatomical-structures',
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './anatomical-structures.component.html',
-  styleUrl: './anatomical-structures.component.scss'
+  styleUrl: './anatomical-structures.component.scss',
 })
 export class AnatomicalStructuresComponent {
+  uniqueStructures: Structure[] = [];
 
+  constructor(private anatomicalStructureService: AnatomicalStructureService) {}
+
+  ngOnInit() {
+    this.anatomicalStructureService.fetchData().subscribe((data) => {
+      const structures: Structure[] = data.data
+        .map((row: any) => row.anatomical_structures)
+        .flat();
+
+      const uniqueNames = new Set(
+        structures
+          .filter((s): s is Structure => s?.name != null)
+          .map((s) => s.name)
+      );
+
+      this.uniqueStructures = Array.from(uniqueNames)
+        .map((name) => structures.find((s) => s.name === name)!)
+        .filter((s): s is Structure => s !== undefined);
+
+      console.log(this.uniqueStructures);
+    });
+  }
+
+  onStructureClick(structure: Structure) {
+    console.log('Clicked structure:', structure);
+  }
 }
