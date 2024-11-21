f53b7131feb610795e2828fd494d37d5aa4bcb31
Prashant Channe <prashantchanne121@gmail.com>
2024-11-21 15:49:46 -0500

    updated styles for dialog

--- src/app/components/anatomical-structure-details/anatomical-structure-details.component.ts
+++ src/app/components/anatomical-structure-details/anatomical-structure-details.component.ts
@@ -3,13 +3,28 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { StructureDetails } from '../../models/anatomical-structure-detail-model';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-anatomical-structure-details',
   templateUrl: './anatomical-structure-details.component.html',
   styleUrls: ['./anatomical-structure-details.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatDialogModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatTooltipModule,
+  ],
 })
 export class AnatomicalStructureDetailsComponent {
   constructor(
